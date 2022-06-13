ac1bfbd76fc35286413a9f67bd0e652bf7dbd59e
taha kourani <tahakourani88@gmail.com>
2022-06-13 12:53:47 +0300

    first web file

---
+++ scipt.js
@@ -0,0 +1,91 @@
+var taha8 = document.getElementById("taha8")
+var taha6 = document.getElementById("taha6")
+taha8.onclick = function taha8() {
+    swal({
+        title: "Good job!",
+        text: "welcome in my web",
+        icon: "success",
+        button: "hello Dears",
+    });
+}
+taha6.onclick = function() {
+    swal({
+        title: "Good job!",
+        text: "welcome in my web",
+        icon: "success",
+        button: "hello Dears",
+    });
+}
+var taha4 = document.getElementById("taha4")
+var taha3 = document.getElementById("taha3")
+var taha2 = document.getElementById("taha2")
+taha4.onclick = function() {
+    swal({
+        title: "Good job!",
+        text: "welcome in my web",
+        icon: "success",
+        button: "hello Dears",
+    });
+}
+taha3.onclick = function() {
+    swal({
+        title: "Good job!",
+        text: "welcome in my web",
+        icon: "success",
+        button: "hello Dears",
+    });
+}
+taha2.onclick = function() {
+    swal({
+        title: "Good job!",
+        text: "welcome in my web",
+        icon: "success",
+        button: "hello Dears",
+    });
+}
+var taha1 = document.getElementById("taha1");
+taha1.onclick = function() {
+    swal({
+        title: "Good job!",
+        text: "welcome in my web",
+        icon: "success",
+        button: "hello Dears",
+    });
+}
+var taha0 = document.getElementById("taha0")
+taha0.onclick = function() {
+    console.log("my name is taha and iam programmer")
+    swal({
+        title: "Good job!",
+        text: "welcome in my web",
+        icon: "success",
+        button: "hello Dears",
+    });
+}
+var taha0000 = document.getElementById("taha0000");
+var taha000 = document.getElementById("taha000");
+var taha00 = document.getElementById("taha00");
+taha0000.onclick = function() {
+    swal({
+        title: "Good job!",
+        text: "welcome in my web",
+        icon: "success",
+        button: "hello Dears",
+    });
+}
+taha000.onclick = function() {
+    swal({
+        title: "Good job!",
+        text: "welcome in my web",
+        icon: "success",
+        button: "hello Dears",
+    });
+}
+taha00.onclick = function() {
+    swal({
+        title: "Good job!",
+        text: "welcome in my web",
+        icon: "success",
+        button: "hello Dears",
+    });
+}
